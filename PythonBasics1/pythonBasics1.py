d18b4cd816dc83948ea998669a44c285007d0e7a
Jacob Woodhouse <jwoodhou@uncc.edu>
2020-10-13 17:38:45 -0400

    Activity 5.0 initial commit

--- PythonBasics1/pythonBasics1.py
+++ PythonBasics1/pythonBasics1.py
@@ -1,26 +1,32 @@
 # Python Activtiy
-# 
-# Fill in the code for the functions below. 
+#
+# Fill in the code for the functions below.
 # The starter code for each function includes a 'return'
 # which is just a placeholder for your code.
 
 # Part A. starts_with
 # Define a function starts_with(s, char) that takes a string and a character
-# and returns true if the string starts with that character and false otherwise. 
+# and returns true if the string starts with that character and false otherwise.
 def starts_with(s, char):
-  # YOUR CODE HERE
-  
-  return
+    check = False
+    if s == '' and char == '':
+        return True
+    if len(s) > 0:
+        if s[0] == char:
+            check = True
+        else:
+            check = False
+    return check
 
 # Part B. starts_with_vowel
 # Define a function starts_with_vowel(s) that takes a string and
-# returns true if the string starts with a vowel and false otherwise. 
+# returns true if the string starts with a vowel and false otherwise.
 # For our purposes, a consonant is any letter other than A, E, I, O, U)
-# Your solution should work for both upper and lower cases 
+# Your solution should work for both upper and lower cases
 def starts_with_vowel(s):
-  # YOUR CODE HERE
-  
-  return
+    vowels = ('a','e','i','o','u','A','E','I','O','U')
+    checkFirstLetter = s.startswith(vowels)
+    return checkFirstLetter
 
 # Part C. max_min_sum
 # Define a function max_min_sum(arr) that takes an array and returns the sum
@@ -28,6 +34,11 @@ def starts_with_vowel(s):
 # For an empty array it should return zero
 # For an array with just one element, it should return that element
 def max_min_sum(arr):
-  # YOUR CODE HERE
-  
-  return
+    if len(arr) == 0:
+        return 0
+    if len(arr) == 1:
+        return arr[0]
+    if len(arr) > 1:
+        m = max(arr)
+        n = min(arr)
+        return m + n
