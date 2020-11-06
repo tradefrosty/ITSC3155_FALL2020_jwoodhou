df23c5425867464f7dccec7c0ed680da7e1e5ae6
Jacob Woodhouse <jwoodhou@uncc.edu>
2020-11-06 18:13:28 -0500

    flask app #3 - added new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -31,6 +31,11 @@ def get_note(note_id):
 
     return render_template('note.html', note = notes[int(note_id)])
 
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name': 'Jacob', 'email': 'jwoodhou@uncc.edu'}
+    return render_template('new.html', user=a_user)
+
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
