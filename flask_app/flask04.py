1605c1a4393d337fdf59b6181605b3b90724c5bf
Jacob Woodhouse <jwoodhou@uncc.edu>
2020-11-13 17:31:29 -0500

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -30,22 +30,26 @@ notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
-    a_user = {'name': 'Jacob', 'email':'jwoodhou@uncc.edu'}
-
+    a_user = db.session.query(User).filter_by(email='jwoodou@uncc.edu')
+    
     return render_template("index.html", user = a_user)
 
 @app.route('/notes')
 def get_notes():
-    a_user = {'name': 'Jacob', 'email': 'jwoodhou@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='jwoodou@uncc.edu')
+    my_notes = db.session.query(Note).all()
 
-    return render_template('notes.html', notes = notes, user = a_user)
+    return render_template('notes.html', notes = my_notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Jacob', 'email': 'jwoodhou@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='jwoodou@uncc.edu')
+    my_note = db.session.query(Note).filter_by(id=note_id)
+
+   
     
 
-    return render_template('note.html', note = notes[int(note_id)], user = a_user)
+    return render_template('note.html', note = my_note, user = a_user)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -57,11 +61,13 @@ def new_note():
         from datetime import date
         today = date.today()
         today = today.strftime("%m-%d-%y")
-        id = len(notes)+1
-        notes[id] = {'title': title, 'text': text, 'date': today}
-        request_data = request.form
+        newEntry = Note(title, text, today)
+        db.session.add(newEntry)
+        db.session.commit()
         return redirect(url_for('get_notes',name = a_user))
+        
     else:
+        a_user = db.session.query(User).filter_by(email='jwoodou@uncc.edu')
         return render_template('new.html', user=a_user) 
 
 
