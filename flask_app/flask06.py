cb799cbab0dcda336a293a159ed233ae4bf83bfa
Jacob Woodhouse <jwoodhou@uncc.edu>
2020-12-07 23:43:40 -0500

    added logout functionality and updated user navigation links

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -153,6 +153,14 @@ def login():
         # form did not validate or GET request
         return render_template("login.html", form=login_form)
 
+@app.route('/logout')
+def logout():
+    # check if a user is saved in session
+    if session.get('user'):
+        session.clear()
+
+    return redirect(url_for('index'))
+
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
 
